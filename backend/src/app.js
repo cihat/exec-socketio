ae618ba62ef300dbb3a13733db75655ef30bd8fc
cihat-salik <cihat@jotform.com>
2023-01-16 21:00:54 +0300

    Add `cors and socket.io` packages

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,13 +3,23 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
+const frontendGuiRouter = require('./routes/frontend-gui')
 
-require('./database-connection')
+// require('./database-connection')
 
 const app = express()
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+)
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -23,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+app.use('/frontend-gui', frontendGuiRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
